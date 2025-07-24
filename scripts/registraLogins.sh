1e2b9cc0fa8882f3140335324f0721686853200a
Ana Luiza Martins Karl <45290513+aluizakarl@users.noreply.github.com>
2025-07-24 13:31:36 -0300

    updating registraLogins.sh

--- scripts/registraLogins.sh
+++ scripts/registraLogins.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Registra logins recentes e salva em um arquivo de log
 
-mkdir $HOME/logs/
+if [ ! -e $HOME/logs ]; then
+	mkdir $HOME/logs
+fi
 
 echo "Logins em $(date)" >> $HOME/logs/login.log
 last -a | head -n 5 >> $HOME/logs/login.log
